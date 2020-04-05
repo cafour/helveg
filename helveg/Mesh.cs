b929fe827a8117a01dcdfb8c4d304a9ef921dc49
Adam Štěpánek <xstepan1@fi.muni.cz>
2020-04-05 08:28:44 +0200

    Rename vkinterop to helveg

--- helveg/Mesh.cs
+++ helveg/Mesh.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Numerics;
 
-namespace VkInterop
+namespace Helveg
 {
     public struct Mesh
     {
--- helveg/Program.cs
+++ helveg/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Numerics;
 using System.Runtime.InteropServices;
 
-namespace VkInterop
+namespace Helveg
 {
     public class Program
     {
