afcde33360f4ab366f7db05b81e6d8ed3f9536dd
Adam Štěpánek <xstepan1@fi.muni.cz>
2020-06-19 18:54:13 +0200

    Attempt to build a nuget package on linux

--- runrd.sh
+++ runrd.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-LD_LIBRARY_PATH=~/dev/fi/helveg/build/vku LD_PRELOAD=~/dev/vk-loader/build/loader/libvulkan.so renderdoccmd capture -c rd -w -d . ./build/helveg/helveg world
+LD_LIBRARY_PATH=./build/vku LD_PRELOAD=~/dev/vk-loader/build/loader/libvulkan.so renderdoccmd capture -c rd -w -d . ./build/helveg/helveg world
