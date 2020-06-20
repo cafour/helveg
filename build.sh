1d4add3bae7d88607376304d740ac8bbdc56595f
Adam Štěpánek <xstepan1@fi.muni.cz>
2020-06-20 09:47:29 +0200

    Attempt to write a Dockerfile

--- build.sh
+++ build.sh
@@ -4,28 +4,38 @@ SOURCE_DIR="$PWD"
 BUILD_DIR="$PWD/build"
 ARTIFACTS_DIR="$PWD/artifacts"
 
-windows=0
-install=0
-pack=0
-clean=0
+windows=false
+configure=true
+build=true
+install=false
+pack=false
+clean=false
 version=$(git describe --tags --abbrev=0)
 
 while [[ $# > 0 ]]; do
     case "$1" in
     --windows)
-        windows=1
+        windows=true
+        ;;
+
+    --no-configure)
+        configure=false
+        ;;
+
+    --no-build)
+        build=false
         ;;
 
     --install)
-        install=1
+        install=true
         ;;
 
     --pack)
-        pack=1
+        pack=true
         ;;
 
     --clean)
-        clean=1
+        clean=true
         ;;
 
     --version)
@@ -36,40 +46,41 @@ while [[ $# > 0 ]]; do
   shift
 done
 
-if [ $clean -eq 1 ]; then
+if $clean; then
     echo "Cleaning build directory"
     rm -rf "$BUILD_DIR" 2>/dev/null
     echo "Cleaning artifacts directory"
     rm -rf "$ARTIFACTS_DIR" 2>/dev/null
 fi
-
 if [ ! -d "$BUILD_DIR" ]; then
     mkdir "$BUILD_DIR"
 fi
-
 if [ ! -d "$ARTIFACTS_DIR" ]; then
     mkdir "$ARTIFACTS_DIR"
 fi
 
 CONFIGURE_ARGS="-DCMAKE_INSTALL_PREFIX='$ARTIFACTS_DIR'"
-if [ $windows -eq 1 ]; then
+if $windows; then
     CONFIGURE_ARGS+=" -A x64"
 fi
-echo "Configuring vku"
-cmake -S "$SOURCE_DIR" -B "$BUILD_DIR" $CONFIGURE_ARGS
+if $configure; then
+    echo "Configuring vku"
+    cmake -S "$SOURCE_DIR" -B "$BUILD_DIR" $CONFIGURE_ARGS
+fi
 
 BUILD_ARGS="--config Release"
-echo "Building vku"
-cmake --build "$BUILD_DIR" $BUILD_ARGS
+if $build; then
+    echo "Building vku"
+    cmake --build "$BUILD_DIR" $BUILD_ARGS
+fi
 
-if [ $install -eq 1 ]; then
-    INSTALL_ARGS="--config Release"
-
+INSTALL_ARGS="--config Release"
+if $install; then
     echo "Installing vku"
     cmake --install "$BUILD_DIR" $INSTALL_ARGS
 fi
 
-if [ $pack -eq 1 ]; then
+if $pack; then
     echo "Packing helveg"
     dotnet msbuild "$SOURCE_DIR" \
         -target:Restore,Build,Pack \
