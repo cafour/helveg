29fde70642e899fdf5a33c8648e0a40a20e74dbc
Adam Štěpánek <git@cafour.cz>
2022-09-21 11:20:20 +0200

    Add the Stride code-only sample
    Courtesy of https://github.com/VaclavElias/stride-code-only

--- helveg/StrideProgram.cs
+++ helveg/StrideProgram.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Stride.Core.Mathematics;
+using Stride.Engine;
+using Stride.GameDefaults.ProceduralModels;
+using Stride.GameDefaults.Extensions;
 
 namespace Helveg
 {
@@ -11,6 +15,21 @@ namespace Helveg
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello from StrideProgram!");
+
+            using var game = new Game();
+
+            game.Run(start: Start);
+
+            void Start(Scene rootScene)
+            {
+                game.SetupBase3DScene();
+
+                var entity = game.CreatePrimitive(PrimitiveModelType.Capsule);
+
+                entity.Transform.Position = new Vector3(0, 8, 0);
+
+                entity.Scene = rootScene;
+            }
         }
     }
 }
