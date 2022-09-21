0554fcb0fdaeab6671051fbece9db4bbb949ff1d
Adam Štěpánek <git@cafour.cz>
2022-09-21 10:53:44 +0200

    Add StrideProgram

---
+++ helveg/StrideProgram.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helveg
+{
+    public class StrideProgram
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello from StrideProgram!");
+        }
+    }
+}
