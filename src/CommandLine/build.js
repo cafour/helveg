dbff4f46ce7723027b9e41a723dd933b1d78c0d2
Adam Štěpánek <git@cafour.cz>
2022-12-21 10:20:07 +0100

    Add build.js

---
+++ src/CommandLine/build.js
@@ -0,0 +1,21 @@
+import esbuild from "esbuild";
+import yargs from "yargs";
+
+const args = yargs(process.argv).argv;
+
+const isDebug = args.config === "Debug";
+const isRelease = args.config === "Release";
+const isWatch = args.watch === true;
+
+await esbuild.build({
+    entryPoints: ["Scripts/Index.ts"],
+    format: "esm",
+    bundle: true,
+    outfile: "obj/esbuild/helveg.js",
+    minify: isRelease,
+    incremental: false,
+    sourcemap: isDebug,
+    target: ["es2020"],
+    treeShaking: true,
+    watch: isWatch
+});
