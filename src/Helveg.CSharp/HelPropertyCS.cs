eacc7fae479456865e5b940d8103c313f416bd83
Adam Štěpánek <git@cafour.cz>
2023-01-29 17:29:37 +0100

    Add more props to HelPropertyCS

--- src/Helveg.CSharp/HelPropertyCS.cs
+++ src/Helveg.CSharp/HelPropertyCS.cs
@@ -1,3 +1,5 @@
+using System.Collections.Immutable;
+
 namespace Helveg.CSharp;
 
 public record HelPropertyCS : HelSymbolBaseCS
@@ -11,4 +13,22 @@ public record HelPropertyCS : HelSymbolBaseCS
     public HelMethodCS? GetMethod { get; init; }
 
     public HelMethodCS? SetMethod { get; init; }
+
+    public bool IsIndex { get; init; }
+
+    public bool IsReadOnly => GetMethod is not null && SetMethod is null;
+
+    public bool IsWriteOnly => GetMethod is null && SetMethod is not null;
+
+    public bool IsRequired { get; init; }
+
+    public HelRefKindCS RefKind { get; init; }
+
+    public bool ReturnsByRef => RefKind == HelRefKindCS.Ref;
+
+    public bool ReturnsByRefReadonly => RefKind == HelRefKindCS.RefReadOnly;
+
+    public ImmutableArray<HelParameterCS> Parameters { get; init; } = ImmutableArray<HelParameterCS>.Empty;
+
+    public HelPropertyCS? OverriddenProperty { get; init; }
 }
