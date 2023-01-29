8f38c65e70f4df68e4136b61655d46f51a826bbd
Adam Štěpánek <git@cafour.cz>
2023-01-29 18:14:52 +0100

    Add more props to HelTypeCS

--- src/Helveg.CSharp/HelTypeCS.cs
+++ src/Helveg.CSharp/HelTypeCS.cs
@@ -1,19 +1,59 @@
 using System.Collections.Immutable;
-using System.Text.Json.Serialization;
 
 namespace Helveg.CSharp;
 
+// Merges ITypeSymbol, INamedTypeSymbol
 public record HelTypeCS : HelSymbolBaseCS
 {
     public static readonly HelTypeCS Invalid = new();
 
-    public ImmutableArray<IHelSymbolCS> Members { get; init; } = ImmutableArray.Create<IHelSymbolCS>();
+    public ImmutableArray<IHelSymbolCS> Members { get; init; } = ImmutableArray<IHelSymbolCS>.Empty;
 
-    public HelNullabilityCS NullableAnnotation { get; init; }
+    public ImmutableArray<HelTypeCS> NestedTypes { get; init; } = ImmutableArray<HelTypeCS>.Empty;
 
-    [JsonIgnore]
-    public ImmutableArray<HelTypeCS> NestedTypes { get; init; } = ImmutableArray.Create<HelTypeCS>();
-
-    [JsonIgnore]
     public bool IsNested => ContainingType is not null;
+
+    public HelTypeKindCS TypeKind { get; init; }
+
+    public HelTypeCS? BaseType { get; init; }
+
+    public ImmutableArray<HelTypeCS> Interfaces { get; init; } = ImmutableArray<HelTypeCS>.Empty;
+
+    public ImmutableArray<HelTypeCS> AllInterfaces { get; init; } = ImmutableArray<HelTypeCS>.Empty;
+
+    public bool IsReferenceType { get; init; }
+
+    public bool IsValueType { get; init; }
+
+    public bool IsAnonymousType { get; init; }
+
+    public bool IsTupleType { get; init; }
+
+    public bool IsNativeIntegerType { get; init; }
+
+    public bool IsRefLikeType { get; init; }
+
+    public bool IsUnmanagedType { get; init; }
+
+    public bool IsReadOnly { get; init; }
+
+    public bool IsRecord { get; init; }
+
+    public ImmutableArray<HelTypeParameterCS> TypeParameters { get; init; } = ImmutableArray<HelTypeParameterCS>.Empty;
+
+    public int Arity => TypeParameters.Length;
+
+    public bool IsGenericType => TypeParameters.Length > 0;
+
+    public bool IsUnboundGenericType { get; init; }
+
+    public bool IsImplicitClass { get; init; }
+
+    public ImmutableArray<HelTypeCS> TypeArguments { get; init; } = ImmutableArray<HelTypeCS>.Empty;
+
+    public HelTypeCS? EnumUnderlyingType { get; init; }
+
+    public HelTypeCS ConstructedFrom { get; init; } = HelTypeCS.Invalid;
+
+    public IHelSymbolCS? AssociatedSymbol { get; init; }
 }
---
+++ src/Helveg.CSharp/HelTypeKindCS.cs
@@ -0,0 +1,20 @@
+namespace Helveg.CSharp;
+
+// Based on Microsoft.CodeAnalysis.TypeKind
+public enum HelTypeKindCS
+{
+    Unknown = 0,
+    Array = 1,
+    Class = 2,
+    Delegate = 3,
+    Dynamic = 4,
+    Enum = 5,
+    Error = 6,
+    Interface = 7,
+    Module = 8,
+    Pointer = 9,
+    Struct = 10,
+    TypeParameter = 11,
+    Submission = 12,
+    FunctionPointer = 13
+}
