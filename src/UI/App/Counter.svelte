dd313e8adf77da4ca06d970d45ecedc674c3dfcd
Adam Štěpánek <git@cafour.cz>
2023-03-13 17:40:52 +0100

    Setup browser-sync

--- src/UI/App/Counter.svelte
+++ src/UI/App/Counter.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
   let count: number = 0
   const increment = () => {
-    count += 1
+    count += 2
   }
 </script>
 
--- src/UI/App/build.ts
+++ src/UI/App/build.ts
@@ -2,27 +2,59 @@ import esbuild from "esbuild";
 import esbuildSvelte from "esbuild-svelte";
 import sveltePreprocess from "svelte-preprocess";
 import prerenderPlugin from "./prerender.js";
+import yargs from "yargs";
+import { copy } from 'esbuild-plugin-copy';
 
-await esbuild.build({
-    entryPoints: ["main.client.ts"],
-    entryNames: "helveg",
+const args = <any>yargs(process.argv).argv;
+const isRelease = args["release"] === true;
+let isDebug = args["debug"] === true;
+const isWatch = args["watch"] === true;
+
+if (isRelease && isDebug) {
+    throw new Error("A build can't be both --debug and --release.");
+}
+
+if (!isRelease && !isDebug) {
+    isDebug = true;
+}
+
+const context = await esbuild.context({
+    entryPoints: ["helveg.ts", "index.html"],
     format: "iife",
     outdir: "../obj/esbuild",
     mainFields: ["svelte", "browser", "module", "main"],
-    sourcemap: false,
+    sourcemap: isDebug,
     splitting: false,
     write: true,
     logLevel: "info",
     bundle: true,
+    minify: isRelease,
     plugins: [
         esbuildSvelte({
             preprocess: sveltePreprocess()
         }),
+        // copy({
+        //     assets: {
+        //         from: ["./index.html"],
+        //         to: ["../obj/esbuild/index.html"],
+        //         watch: true
+        //     },
+        //     resolveFrom: "cwd",
+        // })
         // prerenderPlugin()
     ],
     loader: {
-        ".svg": "text"
+        ".svg": "text",
+        ".html": "copy"
     },
     metafile: true
 })
-.catch(() => process.exit(1));
+    .catch(() => process.exit(1));
+
+if (isWatch) {
+    await context.watch()
+        .catch(() => process.exit(1));
+} else {
+    await context.rebuild()
+        .catch(() => process.exit(1));;
+}
