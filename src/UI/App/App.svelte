b24d7bdaf77599e3162cb3be52f155fdd5754cfe
Adam Štěpánek <git@cafour.cz>
2023-03-18 23:23:19 +0100

    Add Diagram.svelte

--- src/UI/App/App.svelte
+++ src/UI/App/App.svelte
@@ -1,14 +1,47 @@
 <script lang="ts">
-  import Counter from "./Counter.svelte";
-  import { AppShell } from '@skeletonlabs/skeleton';
-  import { FileButton } from '@skeletonlabs/skeleton';
+  import { AppShell } from "@skeletonlabs/skeleton";
+  import { TabGroup, Tab } from "@skeletonlabs/skeleton";
+  import Diagram from "Diagram.svelte";
+  
+  enum TabKind {
+    None,
+    Properties,
+    DocumentInfo
+  };
+
+  let currentTab = TabKind.None;
 </script>
 
 <AppShell>
-  <svelte:fragment slot="header">
-    <h1>Helveg</h1>
-  
-    <Counter />
-    <FileButton name="files" button="variant-soft-primary"/>
+  <svelte:fragment slot=pageHeader>
+    <div class=p-2>
+      <h1>Helveg</h1>
+    </div>
+  </svelte:fragment>
+  <svelte:fragment slot=sidebarRight>
+    <TabGroup flex="select-none wm-v"
+              regionList="flex-col"
+              border="border border-surface-400-500-token"
+              active="border-l-4 border-primary-400-500-token"
+              padding="px-1 py-3"
+              class="flex flex-row-reverse">
+      <Tab bind:group={currentTab} name={TabKind.None.toString()} value={TabKind.None} flex=hidden></Tab>
+      <Tab bind:group={currentTab} name={TabKind.Properties.toString()} value={TabKind.Properties}>
+        Properties
+      </Tab>
+      <Tab bind:group={currentTab} name={TabKind.DocumentInfo.toString()} value={TabKind.DocumentInfo}>
+        Document Info
+      </Tab>
+      
+      <svelte:fragment slot="panel">
+        {#if currentTab === TabKind.Properties}
+          (properties)
+        {:else if currentTab === TabKind.DocumentInfo}
+          (document info)
+        {/if}
+      </svelte:fragment>
+      
+      <Diagram />
+    </TabGroup>
   </svelte:fragment>
 </AppShell>
---
+++ src/UI/App/Diagram.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+  import type { Multigraph } from "./model";
+  import { Sigma } from "sigma";
+  import Graph from "graphology";
+  import circular from "graphology-layout/circular";
+  import forceAtlas2 from "graphology-layout-forceatlas2";
+
+  export let dataId = "helveg-data";
+
+  let element: HTMLElement;
+  const dataScript = document.getElementById(dataId);
+  if (dataScript == null) {
+    throw new Error(`Could not find the '${dataId}' element.`);
+  }
+
+  const data = <Multigraph>JSON.parse(dataScript.textContent!);
+  const graph = new Graph();
+  for (const node of data.Nodes) {
+    graph.addNode(node.Id, {
+        label: node.Label || node.Id
+    });
+  }
+  
+  var containsRelation = data.Relations.filter(r => r.Id === "contains")[0];
+  for (const edge of containsRelation.Edges) {
+    graph.addDirectedEdge(edge.Src, edge.Dst);
+  }
+  
+  circular.assign(graph);
+  const settings = forceAtlas2.inferSettings(graph);
+  forceAtlas2.assign(graph, { settings, iterations: 600 });
+
+  onMount(() => {
+    new Sigma(graph, element);
+  });
+</script>
+
+<div bind:this={element} />
