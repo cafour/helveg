2461d796a5844e9b8981d00063345b85a8f6c216
Adam Štěpánek <git@cafour.cz>
2023-04-29 19:48:09 +0200

    Restyle the CSharpKindsSubpanel

--- src/UI/App/components/CSharpKindsSubpanel.svelte
+++ src/UI/App/components/CSharpKindsSubpanel.svelte
@@ -16,40 +16,32 @@
 </script>
 
 <Subpanel name="CSharpKinds" indent={false} class="pb-8">
-    <table>
-        <thead>
-            <tr>
-                <th>&nbsp;</th>
-                <th>Included</th>
-                <th>Auto-expanded</th>
-            </tr>
-        </thead>
-        <tbody>
-            {#each kinds as kind}
-                <tr>
-                    <td class="space-nowrap">
-                        <Icon
-                            name={DefaultEntityKindIcons[kind] ??
-                                FALLBACK_ICON_NAME}
-                        />
-                        {kind}
-                    </td>
-                    <td class="text-center">
-                        <input
-                            type="checkbox"
-                            bind:group={$dataOptions.csharp.includedKinds}
-                            value={kind}
-                        />
-                    </td>
-                    <td class="text-center">
-                        <input
-                            type="checkbox"
-                            bind:group={$dataOptions.csharp.autoExpandedKinds}
-                            value={kind}
-                        />
-                    </td></tr
-                >
-            {/each}
-        </tbody>
-    </table>
+    <div class="flex flex-col gap-8">
+        <div class="flex flex-row gap-4 border-b-1 border-surface-100 justify-content-betweeen flex-grow-1">
+            <strong class=flex-grow-1>&nbsp</strong>
+            <strong>Included</strong>
+            <strong>Auto-expanded</strong>
+        </div>
+        {#each kinds as kind}
+            <div class="flex flex-row flex-grow-1 gap-4 pl-8 pr-8   ">
+                <div class="space-nowrap flex-grow-1">
+                    <Icon
+                        name={DefaultEntityKindIcons[kind] ??
+                            FALLBACK_ICON_NAME}
+                    />
+                    {kind}
+                </div>
+                <input
+                    type="checkbox"
+                    bind:group={$dataOptions.csharp.includedKinds}
+                    value={kind}
+                />
+                <input
+                    type="checkbox"
+                    bind:group={$dataOptions.csharp.autoExpandedKinds}
+                    value={kind}
+                />
+            </div>
+        {/each}
+    </div>
 </Subpanel>
