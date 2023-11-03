f8f2530d0a39f2e610d674ffaa988a278efc85d7
Adam Štěpánek <git@cafour.cz>
2023-11-03 19:28:08 +0100

    Fix structural.ts imports

---
+++ packages/helveg/deps/graphology-layout-forceatlas2.ts
@@ -0,0 +1,3 @@
+export * from "npm:graphology-layout-forceatlas2@0.10.1";
+import forceAtlas2 from "npm:graphology-layout-forceatlas2@0.10.1";
+export default forceAtlas2;
---
+++ packages/helveg/deps/graphology.ts
@@ -0,0 +1,4 @@
+export * from "npm:graphology@0.25.4";
+import Graph from "npm:graphology@0.25.4";
+export default Graph;
+export * from "npm:graphology-layout-forceatlas2@0.10.1";
