774d0476ed6e57ee7185a68ca2272acd775c2bee
Adam Štěpánek <git@cafour.cz>
2023-11-06 21:46:32 +0100

    Add template.html

--- packages/helveg-diagram/mod.ts
+++ packages/helveg-diagram/mod.ts
@@ -1,1 +1,12 @@
-export * from "./model/instance.ts";
+import { HelvegInstance, initializeHelvegInstance } from "./model/instance.ts";
+
+declare global {
+    interface Window {
+        helveg: HelvegInstance;
+    }
+
+    const helveg: HelvegInstance;
+}
+
+initializeHelvegInstance(window.helveg)
+    .catch(console.error);
