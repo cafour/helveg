1cf2138bb007c5fe0ce9718f90b4d68a56e128c7
Adam Štěpánek <git@cafour.cz>
2023-11-10 19:24:48 +0100

    Reset package.json versions

--- packages/helveg-diagram/eng/git-version.ts
+++ packages/helveg-diagram/eng/git-version.ts
@@ -1,15 +1,26 @@
 import util from "util";
 import childProcess from "child_process";
+import yargs from "yargs";
+import { hideBin } from "yargs/helpers";
 
 const exec = util.promisify(childProcess.exec);
 
-const gitVersionPromise = exec("dotnet gitversion /showvariable SemVer");
-const gitVersion = await gitVersionPromise;
-if (gitVersionPromise.child.exitCode !== 0) {
-    throw new Error(`GitVersion returned ${gitVersionPromise.child.exitCode}. Message:\n${gitVersion.stderr ?? gitVersion.stdout}`);
+let version = "0.0.0-local";
+
+const argv = yargs(hideBin(process.argv))
+    .options({
+        unset: { type: "boolean", default: false },
+    }).parseSync();
+
+if (!argv.unset) {
+    const gitVersionPromise = exec("dotnet gitversion /showvariable SemVer");
+    const gitVersion = await gitVersionPromise;
+    if (gitVersionPromise.child.exitCode !== 0) {
+        throw new Error(`GitVersion returned ${gitVersionPromise.child.exitCode}. Message:\n${gitVersion.stderr ?? gitVersion.stdout}`);
+    }
+    version = gitVersion.stdout.trim();
 }
 
-const version = gitVersion.stdout.trim();
 console.log(version);
 
 const npmVersionPromise = exec(`npm version --allow-same-version --no-commit-hooks --no-git-tag-version "${version}"`);
