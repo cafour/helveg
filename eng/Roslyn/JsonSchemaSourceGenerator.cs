b59f229d6572f00e91632e63396539b0994d0fef
Adam Štěpánek <git@cafour.cz>
2023-11-11 12:01:56 +0100

    Add a source generator for the JSON schema

---
+++ eng/Roslyn/JsonSchemaSourceGenerator.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Immutable;
+using System.IO;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Namotion.Reflection;
+using NJsonSchema;
+using NJsonSchema.CodeGeneration.CSharp;
+
+namespace Helveg.Engineering.Roslyn;
+
+[Generator]
+public class JsonSchemaSourceGenerator : ISourceGenerator
+{
+    public void Execute(GeneratorExecutionContext context)
+    {
+        foreach (var file in context.AdditionalFiles)
+        {
+            if (file is not null && context.AnalyzerConfigOptions.GetOptions(file)
+                .TryGetPropertyValue("build_metadata.IsJsonSchema", false))
+            {
+                var sourceText = file.GetText();
+                if (sourceText is null)
+                {
+                    continue;
+                }
+
+                var schema = JsonSchema.FromJsonAsync(sourceText.ToString()).GetAwaiter().GetResult();
+                var generator = new CSharpGenerator(schema);
+                var code = generator.GenerateFile();
+                context.AddSource(Path.GetFileNameWithoutExtension(file.Path), code);
+            }
+        }
+    }
+
+    public void Initialize(GeneratorInitializationContext context)
+    {
+    }
+}
