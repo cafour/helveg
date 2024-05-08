c7c790e1f01e6483323cd2d15364f8b53d38e2b1
Adam Štěpánek <git@cafour.cz>
2024-05-08 15:54:31 +0200

    Start work on hints and tooltips

---
+++ packages/helveg-explorer/components/Tooltip.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+    export let text: string;
+    export let target: HTMLElement
+    
+    let tooltip: HTMLElement;
+    
+    target.addEventListener("mouseover", onMouseover);
+    target.addEventListener("mouseleave", onMouseleave);
+    
+    let timeout = 0;
+    
+    function onMouseover() {
+        timeout = window.setTimeout(() => tooltip.classList.add("visible"), 500);
+    }
+
+    function onMouseleave() {
+        clearTimeout(timeout);
+        tooltip.classList.remove("visible");
+    }
+</script>
+
+<div class="tooltip" bind:this={tooltip}>{text}</div>
