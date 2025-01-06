0f264a304f6d5ce9fce57cb260aad0b4864972af
Adam Štěpánek <git@cafour.cz>
2025-01-06 18:42:01 +0100

    Remove the virtual list
    I couldn't get it to work. :/

--- packages/helveg-explorer/components/TreeView.svelte
+++ packages/helveg-explorer/components/TreeView.svelte
@@ -9,7 +9,6 @@
         getForestItems,
         type HelvegForestItem,
     } from "../deps/helveg-diagram.ts";
-    import TreeViewNode from "./TreeViewNode.svelte";
     import Icon from "./Icon.svelte";
     import VirtualList from "svelte-tiny-virtual-list";
 
@@ -63,45 +62,37 @@
 
     function toggleItem(index: number) {
         items[index].isExpanded = !items[index].isExpanded;
-        virtualList.recomputeSizes(index + 1);
+        // virtualList.recomputeSizes(index + 1);
     }
 </script>
 
 <div class="tree-view {additionalClass} flex flex-row" class:open={isOpen}>
     <Panel id={AppPanels.TreeView} name="Tree View" class="flex-grow-1">
         {#if items.length > 0}
-            <VirtualList
-                bind:this={virtualList}
-                height="100%"
-                itemCount={items.length}
-                itemSize={getItemHeight}
-            >
-                <div
-                    slot="item"
-                    class="tree-view-node flex flex-col gap-2"
-                    let:index
-                    let:style
-                    {style}
-                    class:item-hidden={items[index].parent &&
-                        !items[index].parent?.isExpanded}
-                >
-                    {#if items[index] !== undefined}
+            {#each items as node}
+                {#if node.depth == 0 || (node.parent && node.parent?.isExpanded)}
+                    <div
+                        class="tree-view-node flex flex-col gap-2"
+                        class:item-hidden={node.parent &&
+                            !node.parent?.isExpanded}
+                    >
                         <div
                             class="flex flex-row gap-4 align-items-center"
-                            style="padding-left: {items[index].depth * 0.5}rem;"
+                            style="padding-left: {node.depth * 0.5}rem;"
                         >
-                            {#if items[index].children === undefined || items[index].children?.length == 0}
+                            {#if node.children === undefined || node.children?.length == 0}
                                 <Icon title="" name="vscode:blank" />
                             {:else}
                                 <button
-                                    on:click={() => toggleItem(index)}
+                                    on:click={() =>
+                                        (node.isExpanded = !node.isExpanded)}
                                     class="flex flex-row align-items-center"
                                 >
                                     <Icon
-                                        title={items[index].isExpanded
+                                        title={node.isExpanded
                                             ? "Collapse"
                                             : "Expand"}
-                                        name={items[index].isExpanded
+                                        name={node.isExpanded
                                             ? "vscode:chevron-down"
                                             : "vscode:chevron-right"}
                                     />
@@ -109,19 +100,18 @@
                             {/if}
                             <button
                                 class="flex flex-row gap-4 align-items-center"
-                                on:click={() => onNodeClicked(items[index].id)}
+                                on:click={() => onNodeClicked(node.id)}
                             >
                                 <Icon
-                                    title={items[index].node.kind}
-                                    name={items[index].node.icon ??
-                                        FALLBACK_NODE_ICON}
+                                    title={node.node.kind}
+                                    name={node.node.icon ?? FALLBACK_NODE_ICON}
                                 />
-                                <span>{items[index].node.label}</span>
+                                <span>{node.node.label}</span>
                             </button>
                         </div>
-                    {/if}
-                </div>
-            </VirtualList>
+                    </div>
+                {/if}
+            {/each}
         {:else}
             <i
                 >The displayed graph is empty or no relation is selected. Use
