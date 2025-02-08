5fdc8b34ce68f33f273e8fb5c2136ad96b69592d
Adam Štěpánek <git@cafour.cz>
2025-02-08 19:41:20 +0100

    Work on CheatSheet styling

--- packages/helveg-explorer/components/CheatSheet.svelte
+++ packages/helveg-explorer/components/CheatSheet.svelte
@@ -8,10 +8,23 @@
     export let buttonStyle: string | undefined = undefined;
 </script>
 
-<ButtonIcon class="cheatsheet-button large" icon="vscode:question" on:click={() => (isOpen = !isOpen)} style={buttonStyle} />
+<ButtonIcon
+    class="cheatsheet-button large"
+    icon="vscode:question"
+    on:click={() => (isOpen = !isOpen)}
+    style={buttonStyle}
+/>
 
-<div class="cheatsheet flex flex-col {isOpen ? 'visible' : 'invisible'}">
-    <Dock name="cheatsheet">
+<div class="cheatsheet {isOpen ? 'visible' : 'invisible'}">
+    <Dock
+        name="cheatsheet"
+        class="cheatsheet-inner"
+        direction="flex-col"
+        tabListDirection="row"
+        allowDeselect={false}
+    >
         <Tab name="Controls" value="controls">This will a list of controls.</Tab>
+        <Tab name="Glyphs" value="glyphs">This will a look at glyphs.</Tab>
+        <Tab name="Panels" value="panels">This will a look at panels.</Tab>
     </Dock>
 </div>
--- packages/helveg-explorer/components/Dock.svelte
+++ packages/helveg-explorer/components/Dock.svelte
@@ -42,47 +42,50 @@
     let currentTab = DockContext.currentTab;
     let tabDescriptors = DockContext.tabDescriptors;
 
+    export let allowDeselect: boolean = true;
     onMount(() => {
         const storedTab = localStorage.getItem(`Dock.${name}.currentTab`);
         setTab(storedTab && storedTab.length ? storedTab : fallbackTab);
     });
 
+    
     export function setTab(value: string | null) {
         if (value === $currentTab) {
             return;
         }
 
-        $currentTab = $currentTab === value ? null : value;
+        $currentTab = (allowDeselect && $currentTab === value) ? null : value;
         localStorage.setItem(`Dock.${name}.currentTab`, $currentTab!);
     }
 
     export function toggleTab(value: string) {
-        $currentTab = $currentTab === value ? null : value;
+        $currentTab = (allowDeselect && $currentTab === value) ? null : value;
         localStorage.setItem(`Dock.${name}.currentTab`, $currentTab!);
     }
 
     let className: string | undefined = undefined;
     export { className as class };
+    export let direction: string = "flex-row-reverse";
+    export let tabListDirection: string = "col";
     export let style: string | undefined = undefined;
 </script>
 
-<div class="dock flex flex-row-reverse relative {className}" {style}>
-    <div class="tab-list flex flex-col">
+<div class="dock flex {direction} relative {className}" {style}>
+    <div class="tab-list flex {tabListDirection}">
         {#each $tabDescriptors as tabDescriptor}
             <!-- svelte-ignore a11y-no-static-element-interactions -->
             <div
                 class="tab-item"
                 class:active={$currentTab === tabDescriptor.value}
+                class:with-icon={tabDescriptor.icon != null}
                 on:click={() => toggleTab(tabDescriptor.value)}
                 on:keypress
                 title={tabDescriptor.name}
             >
                 {#if tabDescriptor.icon}
-                    <Icon
-                        name={tabDescriptor.icon}
-                        title={tabDescriptor.name}
-                        theme="helveg"
-                    />
+                    <Icon name={tabDescriptor.icon} title={tabDescriptor.name} theme="helveg" />
+                {:else}
+                    <span>{tabDescriptor.name}</span>
                 {/if}
             </div>
         {/each}
