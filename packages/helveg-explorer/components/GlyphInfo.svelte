94fc56fdaa3280cd14d7a10bb5240871a8bb70ab
Adam Štěpánek <git@cafour.cz>
2025-02-09 17:20:42 +0100

    Remove old text from GlyphInfo

--- packages/helveg-explorer/components/GlyphInfo.svelte
+++ packages/helveg-explorer/components/GlyphInfo.svelte
@@ -149,24 +149,3 @@
         </div>
     </div>
 </div>
-<p>
-    Glyphs are nodes of the diagram. The icon in the center represents the kind of the visualized entity. The outlines
-    symbolize, in order from innermost to outermost:
-</p>
-<ul>
-    <li>
-        <strong>"Staticness"</strong>: If the line is dashed, the entity is static; if it is solid, it is tied to an
-        instance.
-    </li>
-    <li>
-        <strong>Instance member count</strong>: The number of instance members inside a type entity. Always solid.
-    </li>
-    <li>
-        <strong>Static member count</strong>: The number of static members inside a type entity. Always dashed.
-    </li>
-</ul>
-
-<p>
-    If a node is on fire, there's an error in the entity it represents. The error is shown in the Properties panel.
-    Similarly, if a node is smoldering, there's a warning.
-</p>
