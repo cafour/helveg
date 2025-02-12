1474a7fffebb3cae67baf003c8b47878d2b34b43
Adam Štěpánek <git@cafour.cz>
2025-02-12 23:23:13 +0100

    Mention in the Tutorial that Refresh restores removed nodes

--- packages/helveg-explorer/tutorial.ts
+++ packages/helveg-explorer/tutorial.ts
@@ -26,110 +26,117 @@ export const TUTORIAL_MESSAGES: TutorialMessage[] = [
         message: "Double click on nodes to expand or collapse them.",
         position: {
             bottom: "6rem",
-            left: "4rem"
-        }
+            left: "4rem",
+        },
     },
     {
         header: "Controls: Move nodes",
         message: "Drag nodes around with the left mouse button while holding Shift.",
         position: {
             bottom: "6rem",
-            left: "4rem"
-        }
+            left: "4rem",
+        },
     },
     {
         header: "Controls: Remove nodes",
         message: "Left click on nodes while holding Alt to remove them from the current graph.",
         position: {
             bottom: "6rem",
-            left: "4rem"
-        }
+            left: "4rem",
+        },
     },
     {
         header: "Collapsed node indicator",
-        message: "Notice that nodes that are collapsed and may be expanded have a crescent shadow below them.",
+        message: "Notice that nodes that are collapsed and may be expanded have a shadow below them.",
         position: {
             bottom: "6rem",
-            left: "4rem"
-        }
+            left: "4rem",
+        },
     },
     {
         header: "Toolbar",
-        message: "The Toolbar contains basic graph operations. Try them out!",
+        message:
+            "The Toolbar contains basic graph operations. Try them out! Note that Refresh can be used to restore nodes that you previously deleted.",
         position: {
             elementQuery: ".toolbar",
-        }
+        },
     },
     {
         header: "Search Panel",
         message: "The Search panel allows you to filter the graph using a full-text search or various filters.",
         position: {
             bottom: "6rem",
-            left: "4rem"
-        }
+            left: "4rem",
+        },
     },
     {
         header: "Layout Panel",
         message: "The Layout panel controls which nodes and relations are visualized and laid out.",
         position: {
             bottom: "6rem",
-            left: "4rem"
-        }
+            left: "4rem",
+        },
     },
     {
         header: "Appearance Panel",
-        message: "The Appearance panel can tweak colors and other visual elements. Especially take a look at the Node color preset.",
+        message:
+            "The Appearance panel can tweak colors and other visual elements. Especially take a look at the Node color preset.",
         position: {
             bottom: "6rem",
-            left: "4rem"
-        }
+            left: "4rem",
+        },
     },
     {
         header: "Tools Panel",
-        message: "The Tools panel controls the behavior of various tools. You can for example use it to highlight entire node subtrees.",
+        message:
+            "The Tools panel controls the behavior of various tools. You can for example use it to highlight entire node subtrees.",
         position: {
             bottom: "6rem",
-            left: "4rem"
-        }
+            left: "4rem",
+        },
     },
     {
         header: "Properties Panel",
         message: "The Properties panel shows the properties of the currently selected node. It also shows its preview.",
         position: {
             bottom: "6rem",
-            left: "4rem"
-        }
+            left: "4rem",
+        },
     },
     {
         header: "Document Panel",
-        message: "In the Document panel, you may Reset options of the entire application, or export the graph as an image.",
+        message:
+            "In the Document panel, you may Reset options of the entire application, or export the graph as an image.",
         position: {
             bottom: "6rem",
-            left: "4rem"
-        }
+            left: "4rem",
+        },
     },
     {
         header: "Hints",
-        message: "There are various Hints throughout the panels. To see them hover your mouse over buttons tiny questionmark icons.",
+        message:
+            "There are various Hints throughout the panels. To see them hover your mouse over buttons tiny questionmark icons.",
         position: {
             bottom: "6rem",
-            left: "4rem"
-        }
+            left: "4rem",
+        },
     },
     {
         header: "Tree View",
-        message: "On the left side of the screen there is the TreeView. It shows a more traditional view of the codebase's hierarchy.",
+        message:
+            "On the left side of the screen there is the TreeView. It shows a more traditional view of the codebase's hierarchy.",
         position: {
             bottom: "6rem",
-            left: "4rem"
-        }
+            left: "4rem",
+        },
     },
     {
         header: "Cheat Sheet",
-        message: "Finally, in the lower left corner, there is the Cheat Sheet button. Use it to open up an overview of all the controls, glyphs features, panels, and so on. Good luck!",
+        message:
+            "Finally, in the lower left corner, there is the Cheat Sheet button. Use it to open up an overview of all the controls, glyphs features, panels, and so on. Good luck!",
         position: {
             bottom: "6rem",
-            left: "4rem"
-        }
+            left: "4rem",
+        },
     },
 ];
