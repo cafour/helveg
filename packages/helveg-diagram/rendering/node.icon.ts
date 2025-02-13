efa03f7ca7cbfa7539aa2b36ea11247154b4f2c3
Adam Štěpánek <git@cafour.cz>
2025-02-13 22:58:49 +0100

    Use GL_LINEAR for the node icons

--- packages/helveg-diagram/rendering/node.icon.ts
+++ packages/helveg-diagram/rendering/node.icon.ts
@@ -129,7 +129,7 @@ export class IconProgram extends HelvegNodeProgram<typeof UNIFORMS[number]> {
     private rebindTexture(iconAtlas: Readonly<IconAtlas>, gl: WebGLRenderingContext) {
         gl.bindTexture(gl.TEXTURE_2D, this.texture);
         gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, iconAtlas.texture);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_LINEAR);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
